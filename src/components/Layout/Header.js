29d0d9811674b0a45512c5cf76fb7061ff196ab8
Hung Tran <mhungict99@gmail.com>
2021-08-31 12:50:23 +0700

    fix some header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,6 +8,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { categoryService } from 'data-services/category';
 import { useRouter } from 'next/router'
 import Sidebar from './Sidebar';
+import { Container } from 'react-bootstrap';
 
 
 const customStyles = {
@@ -166,79 +167,83 @@ const Header = () => {
                     </Link>
                 </div>
             </div>
+            <Container>
+                <div className="header-body">
+                    <Link href="/">
+                        <div className="logo">
+                            <Image src={ImagesPath.LOGO} alt="Trang Chủ" />
+                        </div>
 
-            <div className="header-body">
-                <Link href="/">
-                    <div className="logo">
-                        <Image src={ImagesPath.LOGO} alt="Trang Chủ" />
-                    </div>
-
-                </Link>
-                <div className="nav">
-                    <div className='nav-item'>
-                        <span className='title'>Sản Phẩm</span>
-                        <div className='submenu submenu_max_width'>
-                            {
-                                sanpham.map(item => {
-                                    return (
-                                        <div className="menu_list">
-                                            <Link href="#"><div className="title">{item.title}</div></Link>
-                                            {item.childs.map(sp => {
-                                                return (
-                                                    <Link href="#"><div className="subTitle">{sp}</div></Link>
-                                                )
-                                            })}
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    </div>
-                    <div className='nav-item'>
-                        <span className='title'>Tư vấn</span>
-                    </div>
-                    <div className='nav-item'>
-                        <span className='title'>Công trình</span>
-                    </div>
-                    <div className='nav-item'>
-                        <span className='title'>Liên Hệ</span></div>
-                    <div className='nav-item'>
-                        <span className='title'>Thư Viện Ảnh</span>
-                        <div className='submenu'>
-                            <Link href="#"><div className="single_list">Rèm vải</div></Link>
-                            <Link href="#"><div className="single_list">Rèm cầu vồng</div></Link>
-                            <Link href="#"><div className="single_list">Rèm Gỗ</div></Link>
-                            <Link href="#"><div className="single_list">Rèm văn phòng</div></Link>
-                        </div>
-                    </div>
-                    <div className="search">
-                        <div className="search-icon" onClick={toggleSearhBar}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                            </svg>
-                        </div>
-                        <div style={{ fontWeight: 'bold' }}>Tìm Kiếm</div>
-                        <div className="search-bar" style={{ display: searchBarOpen ? 'block' : 'none' }}>
-                            <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "10px", color: "#d61c1f", fontWeight: 'bold' }}>
-                                <span>TÌM KIẾM</span>
-                                <FontAwesomeIcon onClick={toggleSearhBar} icon={faTimes}></FontAwesomeIcon>
-                            </div>
-                            <div style={{ height: '40px' }}>
-                                <select style={{ height: 'inherit', border: "0.5px solid gray", width: "80px", outline: 'none' }}>
-                                    <option>Tất cả</option>
-                                </select>
-                                <input style={{ height: 'inherit', border: "0.5px solid gray", width: "240px", fontSize: 'initial' }} type="text" placeholder="Tìm kiếm sản phẩm..." />
-                                <span style={{ height: 'inherit', display: 'inline-block', width: '40px', borderRadius: '5px', background: '#22232b', textAlign: 'center', lineHeight: '40px', color: 'white' }}>
-                                    <FontAwesomeIcon icon={faSearch} />
-                                </span>
+                    </Link>
+                    <div className="nav">
+                        <div className='nav-item'>
+                            <span className='title'>Sản Phẩm</span>
+                            <div className='submenu submenu_max_width'>
+                                {
+                                    sanpham.map(item => {
+                                        return (
+                                            <div className="menu_list">
+                                                <Link href="#"><div className="title">{item.title}</div></Link>
+                                                {item.childs.map(sp => {
+                                                    return (
+                                                        <Link href="#"><div className="subTitle">{sp}</div></Link>
+                                                    )
+                                                })}
+                                            </div>
+                                        )
+                                    })
+                                }
                             </div>
                         </div>
+                        <div className='nav-item'>
+                            <span className='title'>Tư vấn</span>
+                        </div>
+                        <div className='nav-item'>
+                            <span className='title'>Công trình</span>
+                        </div>
+                        <div className='nav-item'>
+                            <span className='title'>Liên Hệ</span></div>
+                        <div className='nav-item'>
+                            <span className='title'>Thư Viện Ảnh</span>
+                            <div className='submenu'>
+                                <Link href="#"><div className="single_list">Rèm vải</div></Link>
+                                <Link href="#"><div className="single_list">Rèm cầu vồng</div></Link>
+                                <Link href="#"><div className="single_list">Rèm Gỗ</div></Link>
+                                <Link href="#"><div className="single_list">Rèm văn phòng</div></Link>
+                            </div>
+                        </div>
+                        <div className="search">
+                            <div className="search-icon" onClick={toggleSearhBar}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                </svg>
+                            </div>
+                            <div style={{ fontWeight: 'bold' }}>Tìm Kiếm</div>
+                            <div className="search-bar" style={{ display: searchBarOpen ? 'block' : 'none' }}>
+                                <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "10px", color: "#d61c1f", fontWeight: 'bold' }}>
+                                    <span>TÌM KIẾM</span>
+                                    <FontAwesomeIcon onClick={toggleSearhBar} icon={faTimes}></FontAwesomeIcon>
+                                </div>
+                                <div style={{ height: '40px' }}>
+                                    <select style={{ height: 'inherit', border: "0.5px solid gray", width: "80px", outline: 'none' }}>
+                                        <option>Tất cả</option>
+                                    </select>
+                                    <input style={{ height: 'inherit', border: "0.5px solid gray", width: "240px", fontSize: 'initial' }} type="text" placeholder="Tìm kiếm sản phẩm..." />
+                                    <span style={{ height: 'inherit', display: 'inline-block', width: '40px', borderRadius: '5px', background: '#22232b', textAlign: 'center', lineHeight: '40px', color: 'white' }}>
+                                        <FontAwesomeIcon icon={faSearch} />
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="toggle_mobile">
+                        <label for="toogleSidebar"><FontAwesomeIcon icon={faBars} /></label>
                     </div>
                 </div>
-                <div className="toggle_mobile">
-                    <label for="toogleSidebar"><FontAwesomeIcon icon={faBars} /></label>
-                </div>
-            </div>
+            </Container>
+
+
             <input id='toogleSidebar' type='checkbox' style={{ display: 'none' }} />
             <Sidebar />
         </header >
