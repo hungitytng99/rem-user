8321c8492a7354a61e44de9ff72a688d47895a9a
quyenld9699 <quyenld9699@gmail.com>
2021-09-01 17:38:47 +0700

    homepage gần done

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,16 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Link from 'next/link'
 import CardWithTitle from 'ui-source/Card/CardWithTitle'
 import CardReview from 'ui-source/Card/CardReview'
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faChevronRight, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { categoryService } from 'data-services/category'
 import { productService } from 'data-services/product'
 import CardHotProduct from 'ui-source/Card/CardHotProduct'
 import CardPolicy from 'ui-source/Card/CardPolicy'
+import { useState } from 'react'
 
 const attr = {
-    img: "https://i.imgur.com/s8dwMxW.jpeg",
+    img: "https://remcuahoanggia.vn/wp-content/uploads/2019/09/rem-cua-vung-tau.jpg",
     title: "So sánh các loại đồ gỗ được ",
     desc: "Đặc biệt nhờ sự sang trọng đẳng cấp mà khó loại rèm cao cấp nào sánh kịp, rèm gỗ vô cùng phù hợp với những ô cửa sổ kính và phong cách nội thất hiện đại.",
     date: "09/06/2025",
@@ -36,8 +37,50 @@ const attr3 = {
     desc: "Hơn 10 năm kinh nghiệm, tỉ mỉ trong từng đường may mũi chỉ",
 
 }
+
+function SwitchOptionFilter(props) {
+    const { data } = props;
+    switch (data) {
+        case 1:
+            return (
+                <Link href="#" passHref>
+                    <a>Rèm vải một màu {'>>'}</a>
+                </Link>
+            )
+        case 2:
+            return (
+                <Link href="#" passHref>
+                    <a>Rèm cầu vồng {'>>'}</a>
+                </Link>
+            )
+        case 3:
+            return (
+                <Link href="#" passHref>
+                    <a>Rèm gỗ {'>>'}</a>
+                </Link>
+            )
+        case 4:
+            return (
+                <Link href="#" passHref>
+                    <a>Rèm cuốn {'>>'}</a>
+                </Link>
+            )
+        case 5:
+            return (
+                <Link href="#" passHref>
+                    <a>Giàn phơi thông minh {'>>'}</a>
+                </Link>
+            )
+        default:
+            return ""
+    }
+}
 export default function Home(props) {
     const { listHotProduct, listCategoryWithProduct } = props;
+    const [filterProduct, setfilterProduct] = useState(1)
+    function changeFilterProduct(num) {
+        setfilterProduct(num)
+    }
     return (
         <>
             <Head>
@@ -45,8 +88,21 @@ export default function Home(props) {
             </Head>
             <Layout>
                 <div className="home">
+                    <Container>
+                        <div className="home_search_bar">
+                            <div style={{ height: '40px', display: 'flex' }}>
+                                <select style={{ height: 'inherit', border: "0.5px solid gray", width: "90px", outline: 'none' }}>
+                                    <option>Tất cả</option>
+                                </select>
+                                <input style={{ height: 'inherit', border: "0.5px solid gray", width: "calc(100% - 131px)", fontSize: 'initial', padding: "0px 10px" }} type="text" placeholder="Tìm kiếm sản phẩm..." />
+                                <span style={{ height: 'inherit', display: 'inline-block', width: '40px', borderRadius: '5px', background: '#22232b', textAlign: 'center', lineHeight: '40px', color: 'white' }}>
+                                    <FontAwesomeIcon icon={faSearch} />
+                                </span>
+                            </div>
+                        </div>
+                    </Container>
                     <Row className="home__carousel">
-                        <Col xs={12}>
+                        <Col lg={12}>
                             <Carousel
                                 autoPlay={true}
                                 interval={6000}
@@ -70,76 +126,144 @@ export default function Home(props) {
                     <Row>
                         <Container style={{ padding: "0px 30px" }}>
                             <Row style={{ padding: "15px", background: "white" }}>
-                                <Col xs={4}>
+                                <Col lg={4}>
                                     <CardReview {...attr}></CardReview>
                                 </Col>
-                                <Col xs={4}>
+                                <Col lg={4}>
                                     <CardReview {...attr}></CardReview>
                                 </Col>
-                                <Col xs={4}>
+                                <Col lg={4}>
                                     <CardReview {...attr}></CardReview>
                                 </Col>
                             </Row>
                             <Row style={{ margin: "25px 0px" }}>
-                                <Col xs={3}>
+                                <Col lg={3} md={6}>
                                     <CardPolicy {...attr3}></CardPolicy>
                                 </Col>
-                                <Col xs={3}>
+                                <Col lg={3} md={6}>
                                     <CardPolicy {...attr3}></CardPolicy>
                                 </Col>
-                                <Col xs={3}>
+                                <Col lg={3} md={6}>
                                     <CardPolicy {...attr3}></CardPolicy>
                                 </Col>
-                                <Col xs={3}>
+                                <Col lg={3} md={6}>
                                     <CardPolicy {...attr3}></CardPolicy>
                                 </Col>
                             </Row>
                         </Container>
                     </Row>
-                    <Row>
+                    <Row style={{ margin: "25px 0px" }}>
                         <Container>
-                            <h2>SẢN PHẨM BÁN CHẠY</h2>
-                            <h3>Các mẫu rèm cửa được khách hàng hay lựa chọn</h3>
-                            <div>
-                                <span>Rèm vải một màu</span>
-                                <span>Rèm cầu vồng</span>
-                                <span>Rèm Gỗ</span>
-                                <span>Rèm Cuốn</span>
-                                <span>Giàn Phơi Thông Minh</span>
+                            <h2 className="section_title">SẢN PHẨM BÁN CHẠY</h2>
+                            <div className="section_sub_title">Các mẫu rèm cửa được khách hàng hay lựa chọn</div>
+                            <div className="button_filter">
+                                <span className={filterProduct == 1 ? 'active' : ''} onClick={() => changeFilterProduct(1)}>Rèm vải một màu</span>
+                                <span className={filterProduct == 2 ? 'active' : ''} onClick={() => changeFilterProduct(2)}>Rèm cầu vồng</span>
+                                <span className={filterProduct == 3 ? 'active' : ''} onClick={() => changeFilterProduct(3)}>Rèm Gỗ</span>
+                                <span className={filterProduct == 4 ? 'active' : ''} onClick={() => changeFilterProduct(4)}>Rèm Cuốn</span>
+                                <span className={filterProduct == 5 ? 'active' : ''} onClick={() => changeFilterProduct(5)}>Giàn Phơi Thông Minh</span>
                             </div>
                         </Container>
                     </Row>
                     <Row>
-                        <Col xs={4} style={{ marginBottom: "30px" }}>
+                        <Col lg={4} style={{ marginBottom: "30px" }}>
                             <CardHotProduct {...attr}></CardHotProduct>
                         </Col>
-                        <Col xs={4} style={{ marginBottom: "30px" }}>
+                        <Col lg={4} style={{ marginBottom: "30px" }}>
                             <CardHotProduct {...attr}></CardHotProduct>
                         </Col>
-                        <Col xs={4} style={{ marginBottom: "30px" }}>
+                        <Col lg={4} style={{ marginBottom: "30px" }}>
                             <CardHotProduct {...attr}></CardHotProduct>
                         </Col>
-                        <Col xs={4} style={{ marginBottom: "30px" }}>
+                        <Col lg={4} style={{ marginBottom: "30px" }}>
                             <CardHotProduct {...attr}></CardHotProduct>
                         </Col>
                     </Row>
-                    <Row>
-                        <h2>Góc tư vấn</h2>
-                        <h3>Lựa chọn và chăm sóc nhà cửa cùng Rèm The Sun</h3>
+                    <Row style={{ marginBottom: "50px" }}>
+                        <Container>
+                            <p style={{ textAlign: 'center' }}>
+                                Xem tất cả <SwitchOptionFilter data={filterProduct}></SwitchOptionFilter>
+                            </p>
+                        </Container>
                     </Row>
                     <Row>
-                        <Col xs={4} style={{ marginBottom: "30px" }}>
+                        <Col lg={6}>
+                            <CardReview {...attr}></CardReview>
+                        </Col>
+                        <Col lg={6}>
+                            <CardReview {...attr}></CardReview>
+                        </Col>
+                    </Row>
+                    <Row style={{ margin: "50px 0px 25px 0px" }}>
+                        <h2 className="section_title">VIDEO LẮP ĐẶT CÔNG TRÌNH THỰC TẾ</h2>
+                        <div className="section_sub_title">Lắp đặt rèm vải tại nhà khách hàng.</div>
+                    </Row>
+                    <Row style={{ margin: "0px 0px 25px 0px" }}>
+                        <Col lg={6}>
+                            <p style={{ lineHeight: '25.6px' }}>
+                                Rèm cửa đẹp là một trong những yếu tố góp phần tô điểm cho không gian lung linh và để lại ấn tượng với những vị khách mỗi khi đến nhà bạn. Khi một không gian nhà ở đáp ứng đầy đủ nhu cầu về tiện nghi thì vẻ đẹp trên từng ô cửa là vô cùng quan trọng để trở thành một ngôi nhà lý tưởng đáp ứng được cả về mặt thẩm mỹ và công năng. Cùng Rèm The Sun nhìn lại bộ rèm cản sáng được lắp đặt tại nhà khách hàng khu đô thị Geleximco Lê Trọng Tấn.
+                            </p>
+                        </Col>
+                        <Col lg={6}>
+                            <CardReview {...attr}></CardReview>
+                        </Col>
+                    </Row>
+
+                </div>
+
+                <div className="thin_branner" >
+                    <Carousel
+                        autoPlay={true}
+                        interval={6000}
+                        showArrows={false}
+                        infiniteLoop={true}
+                        showThumbs={false}
+                        emulateTouch={true}
+                        numberOfCards={2}
+
+                    >
+                        <div className="thin_branner_img">
+                            <Image layout="fill" objectFit="cover" src={ImagesPath.BRAND_IMG2} alt="giang minh viet banner" />
+                        </div>
+                        <div className="thin_branner_img">
+                            <Image layout="fill" objectFit="cover" src={ImagesPath.BRAND_IMG3} alt="giang minh viet banner handmade" />
+                        </div>
+                        <div className="thin_branner_img">
+                            <Image layout="fill" objectFit="cover" src={ImagesPath.BRAND_IMG4} alt="giang minh viet banner handmade" />
+                        </div>
+                        <div className="thin_branner_img">
+                            <Image layout="fill" objectFit="cover" src={ImagesPath.BRAND_IMG5} alt="giang minh viet banner handmade" />
+                        </div>
+                        <div className="thin_branner_img">
+                            <Image layout="fill" objectFit="cover" src={ImagesPath.BRAND_IMG6} alt="giang minh viet banner handmade" />
+                        </div>
+                        <div className="thin_branner_img">
+                            <Image layout="fill" objectFit="cover" src={ImagesPath.BRAND_IMG7} alt="giang minh viet banner handmade" />
+                        </div>
+                        <div className="thin_branner_img">
+                            <Image layout="fill" objectFit="cover" src={ImagesPath.BRAND_IMG9} alt="giang minh viet banner handmade" />
+                        </div>
+                    </Carousel>
+                </div>
+
+                <div className="home">
+
+                    <Row style={{ margin: "250px 0px 30px 0px" }}>
+                        <h2 className="section_title">Góc tư vấn</h2>
+                        <div className="section_sub_title">Lựa chọn và chăm sóc nhà cửa cùng Rèm The Sun</div>
+                    </Row>
+                    <Row>
+                        <Col md={4} style={{ marginBottom: "30px" }}>
                             <CardProduct {...attr} />
                         </Col>
-                        <Col xs={4} style={{ marginBottom: "30px" }}>
+                        <Col md={4} style={{ marginBottom: "30px" }}>
                             <CardProduct {...attr} />
                         </Col>
-                        <Col xs={4} style={{ marginBottom: "30px" }}>
+                        <Col md={4} style={{ marginBottom: "30px" }}>
                             <CardProduct {...attr} />
                         </Col>
                     </Row>
                     <CardProduct {...attr2} />
-
                 </div>
             </Layout>
         </>
@@ -151,7 +275,7 @@ export async function getServerSideProps() {
     let listHotProduct = [];
     try {
         listCategoryWithProduct = await categoryService.listCategoryWithProduct(
-            { }, { productsPerPage: 8, pageNumber: 1 }
+            {}, { productsPerPage: 8, pageNumber: 1 }
         );
         listHotProduct = await productService.listHotProduct();
         return {
--- src/constants/ImagesPath.js
+++ src/constants/ImagesPath.js
@@ -9,6 +9,14 @@ import news_decorate from 'assets/images/homepage/news/news-decorate.png'
 import news_image from 'assets/images/homepage/news/news-image.jpg'
 import test_url_img from 'assets/images/homepage/products/row1-4.jpg'
 
+import brand_img2 from 'assets/images/homepage/brand/brand_img2.png'
+import brand_img3 from 'assets/images/homepage/brand/brand_img3.png'
+import brand_img4 from 'assets/images/homepage/brand/brand_img4.png'
+import brand_img5 from 'assets/images/homepage/brand/brand_img5.webp'
+import brand_img7 from 'assets/images/homepage/brand/brand_img7.png'
+import brand_img9 from 'assets/images/homepage/brand/brand_img9.webp'
+import brand_img6 from 'assets/images/homepage/brand/brand_img6.webp'
+
 export const ImagesPath = {
     LOGO: logo,
     PRODUCT_6: product_6,
@@ -19,6 +27,13 @@ export const ImagesPath = {
     NEWS_THUMB: news_thumb,
     NEWS_DECORATE: news_decorate,
     NEWS_IMAGE: news_image,
-    URL_TEST: test_url_img
+    URL_TEST: test_url_img,
 
+    BRAND_IMG2: brand_img2,
+    BRAND_IMG3: brand_img3,
+    BRAND_IMG4: brand_img4,
+    BRAND_IMG5: brand_img5,
+    BRAND_IMG6: brand_img6,
+    BRAND_IMG9: brand_img9,
+    BRAND_IMG7: brand_img7,
 }
