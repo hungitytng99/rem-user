801d3357cee78212c03055d6b5dd3c8413d3921b
quyenld9699 <quyenld9699@gmail.com>
2021-09-01 08:45:39 +0700

    sửa server.js

--- server.js
+++ server.js
@@ -11,27 +11,27 @@ app.prepare().then(() => {
     const server = express()
 
     //Tạo ra các router. Dòng này có ý nghĩa khi gửi request đến path /a . Sẽ render file /a.js trong thư mục pages/a.js của Nextjs
-    server.get('/', (req, res) => {
-        return app.render(req, res, '/trang-chu', req.query)
-    })
-    server.get('/', (req, res) => {
-        return app.render(req, res, '/trang-chu', req.query)
-    })
-    server.get('/danh-muc', (req, res) => {
-        return app.render(req, res, '/danh-muc', req.query)
-    })
-    server.get('/lien-he', (req, res) => {
-        return app.render(req, res, '/lien-he', req.query)
-    })
-    server.get('/san-pham', (req, res) => {
-        return app.render(req, res, '/san-pham', req.query)
-    })
-    server.get('/tim-kiem', (req, res) => {
-        return app.render(req, res, '/tim-kiem', req.query)
-    })
-    server.get('/tin-tuc', (req, res) => {
-        return app.render(req, res, '/tim-kiem', req.query)
-    })
+    // server.get('/', (req, res) => {
+    //     return app.render(req, res, '/trang-chu', req.query)
+    // })
+    // server.get('/', (req, res) => {
+    //     return app.render(req, res, '/trang-chu', req.query)
+    // })
+    // server.get('/danh-muc', (req, res) => {
+    //     return app.render(req, res, '/danh-muc', req.query)
+    // })
+    // server.get('/lien-he', (req, res) => {
+    //     return app.render(req, res, '/lien-he', req.query)
+    // })
+    // server.get('/san-pham', (req, res) => {
+    //     return app.render(req, res, '/san-pham', req.query)
+    // })
+    // server.get('/tim-kiem', (req, res) => {
+    //     return app.render(req, res, '/tim-kiem', req.query)
+    // })
+    // server.get('/tin-tuc', (req, res) => {
+    //     return app.render(req, res, '/tim-kiem', req.query)
+    // })
 
 
     // Nếu các bạn muốn các routing tự động liến kết đến route files giống với cấu trúc của Nextjs thì chỉ cần thêm 3 dòng bên dưới
